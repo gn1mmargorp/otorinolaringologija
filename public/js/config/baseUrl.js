3013129fc1f654aafdbf736b38d6f91f962bb341
gn1mmargorp <wely2403@gmail.com>
2024-12-02 06:14:53 +0100

    13

--- public/js/config/baseUrl.js
+++ public/js/config/baseUrl.js
@@ -1,4 +1,10 @@
-const inProduction = process.env.NODE_ENV === 'production';
-const BASE_URL = inProduction ? 'https://vanillajs-online-shop.onrender.com' : 'http://localhost:8080';
+const currentMode = process.env.NODE_ENV;
+let BASE_URL;
+
+if (currentMode === 'production') {
+    BASE_URL = 'https://vanillajs-online-shop.onrender.com';
+} else if (currentMode === 'development') {
+    BASE_URL = 'http://localhost:8080';
+}
 
 export default BASE_URL;
