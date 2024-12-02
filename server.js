d5cc1a9eb273ee4f12e64e0c53a8a266d5e4bd39
gn1mmargorp <wely2403@gmail.com>
2024-12-02 05:35:01 +0100

    11

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.get('/profile', (req, res) => {
 app.get('/product/:id', renderProductPage);
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on PORT: ${PORT}`);
 });
